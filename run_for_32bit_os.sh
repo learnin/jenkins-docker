ee7d100bb30d0768519629fb28d0b5f636446b47
learnin <i.learnin@gmail.com>
2017-03-24 15:49:28 +0900

    Change image/volume/container name

--- run_for_32bit_os.sh
+++ run_for_32bit_os.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# master-data volume
-docker volume create --name jenkinsdocker_master-data
+# app-data volume
+docker volume create --name jenkinsdocker_app-data
 
-# master
+# app
 docker container run \
   -d \
   -e 'JAVA_OPTS=-Djava.awt.headless=true' \
   -e 'TZ=Asia/Tokyo' \
-  --name jenkinsdocker_master_1 \
+  --name jenkinsdocker_app_1 \
   -p 50001:8080 \
   -p 50000:50000 \
   --restart always \
   -v /etc/localtime:/etc/localtime:ro \
-  -v jenkinsdocker_master-data:/var/jenkins_home \
-  jenkinsdocker_master
+  -v jenkinsdocker_app-data:/var/jenkins_home \
+  jenkinsdocker_app
