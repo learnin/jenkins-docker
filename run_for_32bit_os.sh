5df73362338ce9b5dd1f391371d9d4f4a0eaba1e
learnin <i.learnin@gmail.com>
2017-03-24 15:39:38 +0900

    Add docker run restart option

--- run_for_32bit_os.sh
+++ run_for_32bit_os.sh
@@ -11,6 +11,7 @@ docker container run \
   --name jenkinsdocker_master_1 \
   -p 50001:8080 \
   -p 50000:50000 \
+  --restart always \
   -v /etc/localtime:/etc/localtime:ro \
   -v jenkinsdocker_master-data:/var/jenkins_home \
   jenkinsdocker_master
